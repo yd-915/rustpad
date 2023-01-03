faf11ea507e71313351d0142b27b8dd28add9c64
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-01-03 14:35:12 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import init, { set_panic_hook } from "rustpad-wasm";
+import init, { set_panic_hook } from "./rustpad-wasm";
 import App from "./App";
 import "./index.css";
 
